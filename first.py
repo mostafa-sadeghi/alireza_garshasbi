f9b2c4804835059bf9294774ee69a19635fee4a9
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-26 22:01:22 +0330

    first commit

---
+++ first.py
@@ -0,0 +1,18 @@
+# x = 1.0
+# y = 1
+# z = 1 + 3j
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+name1 = 'aaaaa'
+name1 = "12345"
+name2 = 'a'
+print(type(name1))
+print(type(name2))
+# name1[0] = 'b'
+print(name1[0])
+print(name1[1])
+print(name1[2])
+print(name1[3])
+print(name1[4])
