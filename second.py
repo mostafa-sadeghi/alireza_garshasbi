8e3ce025293803bba309fae17ba198a698da035b
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-28 20:53:34 +0330

    first

---
+++ second.py
@@ -0,0 +1,102 @@
+# string = 'abcdefg'
+# slice_str = string[2:6:2]
+# print(slice_str)
+# message = "baallalalla shoul'nt"
+# message = 'baallalalla shoul\'nt'
+# message = '''baallalalla shoul'nt'''
+# message = """baallalalla shoul'nt"""
+# message = '''he said "something"'''
+# message = 'he said "something"'
+# message = "he said \"something\""
+
+# name = input('enter your name ')
+# family = input('enter your family ')
+# message = "hello " + name
+# message = "hello %s %s" % (name, family)
+# message = f"hello {name} {family}"
+# print(message)
+# print(type(name))
+
+
+# x = int(input('enter first number > '))
+# y = int(input('enter second number > '))
+
+# z = x + y
+
+# print(f'{x} + {y} = {z}')
+# x = float(input('enter first number > '))
+# y = float(input('enter second number > '))
+
+# z = x + y
+
+# print(f'{x} + {y} = {z}')
+# x = float(input('enter first number > '))
+# y = float(input('enter second number > '))
+
+# z = int(x) + int(y)
+
+# print(f'{x} + {y} = {z}')
+
+
+# numbers = []
+# print(type(numbers))
+# print(len(numbers))
+# numbers.append("object1")
+# numbers.append("object2")
+# numbers.append("object3")
+# numbers.append("object4")
+# numbers.append(1)
+# numbers.append(True)
+# numbers.append(False)
+# print(numbers)
+# print(numbers[0])
+# print(numbers[6])
+# print(numbers[-1])
+# print(numbers[-2])
+# print(numbers[len(numbers)-1])
+# print(numbers[2:4])
+# x = list(range(1000))
+# print(x[2:-1:2])
+# x = list(range(1000, 10001, 2))
+# print(x)
+# print(f'length is {len(x)}')
+
+
+# my_list = ['a', 'b', 'c']
+# my_list.insert(0, 'A')
+# print(my_list)
+
+# del my_list[0]
+# print(my_list)
+
+# my_list.remove('b')
+# print(my_list)
+
+# my_list.pop()
+# my_list.pop(0)
+# print(my_list)
+# numbers = [1,2,3,1,5,5,5]
+# print(numbers.count(5))
+
+
+# string = '*'
+# string = string + '1'  # string += 1
+# output = '*' * 3
+# print(output)
+
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers += [1]
+# print(numbers)
+
+# numbers = [1, 2]
+# numbers *= 3
+# print(numbers)
+
+
+numbers = [1, 2, 3, 4]
+characters = ['a', 'b']
+
+final_list = [numbers, characters]
+print(final_list)
+print(final_list[1][0])
