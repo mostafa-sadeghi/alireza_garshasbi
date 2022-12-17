13d0d534e9580371ded4159f8623fbf872ab481c
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-17 20:57:58 +0330

    26azar

---
+++ 7.py
@@ -0,0 +1,69 @@
+# print(1 and 2 and 'a' and 'b')
+# import math
+# import random
+
+
+# first import python built-in modules
+# second import third party module
+# third import project modules
+
+# import math
+
+# print(math.ceil(4.2))
+
+
+# import turtle
+
+# s = turtle.Screen()
+
+
+# s.exitonclick()
+
+
+# import sample
+
+# x = sample.add(2, 2)
+# print(x)
+
+# math.sqrt(4)
+# random.randint(1, 100)
+# random.randrange(1, 100)
+
+# pep8
+
+
+# روش های import کردن ماژول ها
+# from sample import *  # forbidden
+# from sample import PI, add
+
+# print(PI)
+# print(add(1, 4))
+
+
+# from sample import X, add, sub
+# X = 90
+# print(PI)
+# print(add(1000, 34))
+
+# print(X)
+
+
+# from sample import mul_numbers as mul
+
+# mul(1,2,3,4)
+
+
+########################################################
+
+
+from sample import *
+# import sample
+# PI = 3
+# sample.add(1, 2)
+# print(sample.PI)
+# print(PI)
+
+
+PI = 3
+print(add(1, 8))
+print(PI)
---
+++ sample.py
@@ -0,0 +1,24 @@
+def add(n1: int, n2: int) -> int:
+    '''
+    This function is used to add two numbers
+    Parameters: n1:int , n2:int
+    Return int
+    '''
+    return n1 + n2
+
+
+PI = 3.14
+X = 100
+
+
+def sub():
+    pass
+
+
+def mul_numbers(*args):
+    pass
+
+# print(__name__)
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(PI)
